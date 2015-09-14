8197a6d09be6a73f60f9d861222b5a50f131b2ca
wangshujie <wangshujie@xiaomi.com>
2015-09-14 18:14:53 +0800

    keylayout: update vendor kl
    Change-Id: I1031fd40706ed03c157dc5e42ac414c8608e0f77
    Signed-off-by: wangshujie <wangshujie@xiaomi.com>

--- device.mk
+++ device.mk
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/goodix.kl:system/usr/keylayout/goodix.kl \
+    $(LOCAL_PATH)/keylayout/atmel-maxtouch.kl:system/usr/keylayout/atmel-maxtouch.kl \
+    $(LOCAL_PATH)/keylayout/ft5x46.kl:system/usr/keylayout/ft5x46.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # Lights
