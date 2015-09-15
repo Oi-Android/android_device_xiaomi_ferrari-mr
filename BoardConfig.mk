db26f8fa07dd43db4b6209f7d4c590300ec3f969
wangshujie <wangshujie@xiaomi.com>
2015-09-15 19:06:12 +0800

    Revert "ferrari: remove custom wlan driver path"
    This reverts commit 62ffa89d32332a428ac1f8d5636d254f45fec60e.
    
    Change-Id: I3a6c210ff7415a116b1bc97e95221b366a7dd600
    Signed-off-by: wangshujie <wangshujie@xiaomi.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,9 @@ BOARD_SEPOLICY_UNION += \
 # Wifi
 TARGET_PROVIDES_WCNSS_QMI := true
 TARGET_USES_QCOM_WCNSS_QMI := true
+# The uncompressed arm64 is too large, split wifi for now
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # inherit from the proprietary version
 -include vendor/xiaomi/ferrari/BoardConfigVendor.mk
