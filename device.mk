565257386da275031fe1851b29df4d4604ad91fb
wangshujie <wangshujie@xiaomi.com>
2015-09-15 19:18:16 +0800

    Revert "tomato: Add support for varying panel densities"
    This reverts commit 63b4f5a4d0b93d9c1cb2000e2f79ad918ae3ade2.
    
    Change-Id: I4b6528d53b9a5b84aa7d12df7e3a63f31ee291d1
    Signed-off-by: wangshujie <wangshujie@xiaomi.com>

--- device.mk
+++ device.mk
@@ -31,12 +31,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Due to multi-density builds, these are set by init
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model ro.sf.lcd_density
-
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1280
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
